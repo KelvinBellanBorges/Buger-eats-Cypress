67526cf1f42ef01761e2e365d0ae4ae2ef86ac24
Kelvin Bellan <kelvin.bellan@gmail.com>
2022-01-19 09:56:55 -0300

    Ajuste no Suite home para os padrões do cypress.json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,7 +2,6 @@
 
 describe('home page', ()=> {
     it('app deve estar online', ()=> {
-        cy.viewport(1440, 900)
         cy.visit('/')
         cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
     })
